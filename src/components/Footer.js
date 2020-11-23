abbecbb771221b9c06ce757258e056119092fdb7
djnokin <nokindj@gmail.com>
2020-11-23 18:33:25 +0000

    done

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+
+function Footer() {
+    return (
+        <div className="container">
+            <img src="./images/icon1.png"/>
+            <h2>Declarative</h2>
+            <p>React makes it<br/>painless to create<br/>interactive UIs. </p>
+            <img src="./images/icon2.png"/>
+            <h2>Components</h2>
+            <p>Build encapsulated<br/>components that<br/>manage their state</p>
+            <img src="./images/icon3.png"/>
+            <h2>Single-Way</h2>
+            <p>A set of immutable<br/>values are passed to<br/>the component's</p>
+            <img src="./images/icon4.png"/>
+            <h2>JSX</h2>
+            <p>Statically-typed,<br/>designed to run on<br/>modern browsers</p>
+        </div>
+    )
+
+}
+
+
+
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+
+function Header() {
+    return (
+        <div className="container">
+        <nav>
+            <img src="./images/ironhack-logo.svg" />
+            <img src="./images/menu-top.svg"/>
+        </nav>
+       <div className="text">
+        <h1>Say hello to<br/>ReactJS</h1>
+        <p>You will learn how to use <br/>
+        the most popular frontend library,<br/>
+        and become a super Ninja developer</p>
+        </div>
+        <div className="awesome">
+        <a href="">Awesome!</a>
+        </div>
+        </div>
+    )
+}
+
+
+export default Header;
+
+
+
+
+
