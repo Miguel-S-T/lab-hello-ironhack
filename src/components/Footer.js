d755157cea03a26bbd7ece10de3f6264120a8fc3
djnokin <nokindj@gmail.com>
2020-11-23 23:59:58 +0000

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,27 @@
 
 function Footer() {
     return (
-        <div className="container">
+        <div className="footer-container">
+            <div className="footer-icons">
             <img src="./images/icon1.png"/>
             <h2>Declarative</h2>
             <p>React makes it<br/>painless to create<br/>interactive UIs. </p>
+            </div>
+            <div className="footer-icons">
             <img src="./images/icon2.png"/>
             <h2>Components</h2>
             <p>Build encapsulated<br/>components that<br/>manage their state</p>
+            </div>
+            <div className="footer-icons">
             <img src="./images/icon3.png"/>
             <h2>Single-Way</h2>
             <p>A set of immutable<br/>values are passed to<br/>the component's</p>
+            </div>
+            <div className="footer-icons">
             <img src="./images/icon4.png"/>
             <h2>JSX</h2>
             <p>Statically-typed,<br/>designed to run on<br/>modern browsers</p>
+            </div>
         </div>
     )
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,20 @@
 
 function Header() {
     return (
-        <div className="container">
+        <div id="header-div">
         <nav>
-            <img src="./images/ironhack-logo.svg" />
-            <img src="./images/menu-top.svg"/>
+            <img id="iron-logo" src="./images/ironhack-logo.svg" />
+            <img id="iron-bar" src="./images/menu-top.svg"/>
         </nav>
        <div className="text">
         <h1>Say hello to<br/>ReactJS</h1>
-        <p>You will learn how to use <br/>
+        <p className="header-parag" >You will learn how to use <br/>
         the most popular frontend library,<br/>
         and become a super Ninja developer</p>
         </div>
-        <div className="awesome">
-        <a href="">Awesome!</a>
-        </div>
+        
+        <button id="awesome-btn" href="">Awesome!</button>
+        
         </div>
     )
 }
